a906354c3c859d69fa1c78dd70863901137ebbda
chang <cshin7374@gmail.com>
2022-05-18 15:10:13 -0500

    commit tree 19b32d86ae144c0151a02f45950bab78fb8565a1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(toDo = 'roller-skate') {
+    return `This Saturday, I want to ${toDo}!`
+}
+
+function mondayWork(toDo = 'go to the office') {
+    return `This Monday, I will ${toDo}.`;
+}
+
+function wrapAdjective(symbol) {
+    return function(adj = 'special') {
+        return `You are ${symbol}${adj}${symbol}!`;
+    }
+}
